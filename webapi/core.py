85f93fd61c32b12175cd8bdc5f4bbab4e66aa5b7
Peter Lauck <griest024@gmail.com>
2022-11-13 14:35:34 -0500

    fix: cookie not getting `SameSite=None` or `Secure` set

--- webapi/core.py
+++ webapi/core.py
@@ -18,6 +18,12 @@ DEFAULT_PREFS = {
 # twisted uses byte strings in deluge version 2.1.0 onward
 use_bytes = version.parse(get_version()) >= version.parse('2.1.0')
 
+# Cookies don't get `SameSite=None` set correctly, we can hack it here
+# see https://github.com/twisted/twisted/issues/10088
+class ForceSecureSameSiteNoneCookieList(list):
+    def append(self, cookie):
+        list.append(self, cookie + b'; Secure; SameSite=None')
+
 class Core(CorePluginBase):
 
     def enable(self):
@@ -61,11 +67,19 @@ class Core(CorePluginBase):
         LOGGER.info('Patching webui for CORS...')
 
         cmp_json = component.get('JSON')
+        cmp_auth = component.get('Auth')
 
+        # JSON
         self.old_render = cmp_json.render
         self.old_send_request = cmp_json._send_response
         cmp_json.render = self.render_patch
         cmp_json._send_response = self._send_response_patch
+
+        # Auth
+        self.old__create_session = cmp_auth._create_session
+        cmp_auth._create_session = self._create_session_patch
+        self.old_check_request = cmp_auth.check_request
+        cmp_auth.check_request = self.check_request_patch
 
         self.patched = True
 
@@ -77,10 +91,30 @@ class Core(CorePluginBase):
         LOGGER.info('Unpatching webui for CORS...')
 
         cmp_json = component.get('JSON')
+        cmp_auth = component.get('Auth')
+
+        # JSON
         cmp_json.render = self.old_render
         cmp_json._send_response = self.old_send_request
+        # Auth
+        cmp_json._create_session = self.old__create_session
+        cmp_json.check_request = self.old_check_request
 
         self.patched = False
+
+    def check_request_patch(self, request, method=None, level=None):
+        request.cookies = ForceSecureSameSiteNoneCookieList()
+
+        result = self.old_check_request(request, method, level)
+
+        return result
+
+    def _create_session_patch(self, request, login='admin'):
+        request.cookies = ForceSecureSameSiteNoneCookieList()
+
+        result = self.old__create_session(request, login)
+
+        return result
 
     def render_patch(self, request):
 
